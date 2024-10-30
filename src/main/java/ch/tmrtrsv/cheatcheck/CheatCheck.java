b452f7426b7e861aed3dc48e986a902f8e84e37d
tmrtrsvch <tmrtrsvch@gmail.com>
2024-10-30 22:40:22 +0200

    Команды от консоли для игрока, который вышел во время проверки

--- src/main/java/ch/tmrtrsv/cheatcheck/CheatCheck.java
+++ src/main/java/ch/tmrtrsv/cheatcheck/CheatCheck.java
@@ -6,6 +6,7 @@ import ch.tmrtrsv.cheatcheck.commands.ReloadCommand;
 import ch.tmrtrsv.cheatcheck.listeners.EventListener;
 import ch.tmrtrsv.cheatcheck.tabcompleters.ReloadTabCompleter;
 import ch.tmrtrsv.cheatcheck.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CheatCheck extends JavaPlugin {
@@ -40,10 +41,10 @@ public class CheatCheck extends JavaPlugin {
     }
 
     private void sendCredit() {
-        getLogger().info(Utils.color(""));
-        getLogger().info(Utils.color("&f &#FF2900C&#FC2600h&#FA2300e&#F72000a&#F41D00t&#F11A00C&#EF1700h&#EC1500e&#E91200c&#E70F00k &#E10900v&#DE06001&#DC0300.&#D900001"));
-        getLogger().info(Utils.color("&f Автор: &#FB3908Т&#FC2B06и&#FD1D04м&#FE0E02у&#FF0000р"));
-        getLogger().info(Utils.color("&f Телеграм: &#008DFF@&#0086FFt&#007FFFm&#0078FFr&#0071FFt&#006BFFr&#0064FFs&#005DFFv&#0056FFc&#004FFFh"));
-        getLogger().info(Utils.color(""));
+        Bukkit.getConsoleSender().sendMessage(Utils.color(""));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("&f &#FF2900C&#FC2600h&#FA2300e&#F72000a&#F41D00t&#F11A00C&#EF1700h&#EC1500e&#E91200c&#E70F00k &#E10900v&#DE06001&#DC0300.&#D900002"));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("&f Автор: &#FB3908Т&#FC2B06и&#FD1D04м&#FE0E02у&#FF0000р"));
+        Bukkit.getConsoleSender().sendMessage(Utils.color("&f Телеграм: &#008DFF@&#0086FFt&#007FFFm&#0078FFr&#0071FFt&#006BFFr&#0064FFs&#005DFFv&#0056FFc&#004FFFh"));
+        Bukkit.getConsoleSender().sendMessage(Utils.color(""));
     }
 }
--- src/main/java/ch/tmrtrsv/cheatcheck/listeners/EventListener.java
+++ src/main/java/ch/tmrtrsv/cheatcheck/listeners/EventListener.java
@@ -2,14 +2,17 @@ package ch.tmrtrsv.cheatcheck.listeners;
 
 import ch.tmrtrsv.cheatcheck.CheatCheck;
 import ch.tmrtrsv.cheatcheck.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -54,6 +57,26 @@ public class EventListener implements Listener {
             Player player = (Player) event.getEntity();
             if (checkedPlayers.contains(player.getUniqueId())) {
                 event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (checkedPlayers.contains(player.getUniqueId())) {
+            checkedPlayers.remove(player.getUniqueId());
+
+            List<String> quitCommands = plugin.getConfig().getStringList("settings.quit_commands");
+            for (String command : quitCommands) {
+                String finalCommand = command.replace("%player%", player.getName());
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), finalCommand);
+            }
+
+            Player checker = Bukkit.getPlayer(plugin.getConfig().getString("settings.checker_name")); // замените "checker_name" на реальное имя проверяющего или UUID
+            if (checker != null && checker.isOnline()) {
+                String message = Utils.color(plugin.getConfig().getString("messages.player_quit").replace("%player%", player.getName()));
+                checker.sendMessage(message);
             }
         }
     }
